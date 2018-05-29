ca33aaf6a135828dd666b67b90e5d33080e32bac
kiling <zqling43@gmail.com>
2018-05-29 20:51:14 +0800

    FCC project added
    FCC project added

---
+++ FCC_Pomodoro_Timer/readme.md
@@ -0,0 +1,8 @@
+## FCC project: Build a Pomodoro Clock
+FCC 链接：[Build a Pomodoro Clock](https://freecodecamp.cn/challenges/build-a-pomodoro-clock)
+
+预览链接：[预览](https://kiling.github.io/Pratices/FCC_Pomodoro_Timer/FCC_Pomodoro_Timer.html)
+
+使用到的库： 
+<br>JS: `jQuery`
+<br>CSS: `Material Design Lite`
---
+++ FCC_Pomodoro_Timer/scripts/pomodoro.js
@@ -0,0 +1,146 @@
+let btnSession = $("#btn-session"),
+    btnBreak = $('#btn-break');
+
+let timer = { // 计时器对象
+    state: '',
+    session: 1,
+    break: 1,
+    loop: true,
+    sessionId: '',
+    breakId: '',
+    isPaused: false,
+    setSession: function () {
+        let tmp = $('#session-text').val();
+        if(timer.checkNum(tmp)) {
+            this.session = Number(tmp);
+        } else {
+            timer.createToast('输入的应为1-60的整数数字');
+        }
+        timer.halt();
+        this.render(this.session, this.break);
+    },
+    setBreak: function () {
+        let tmp = $('#break-text').val();
+        if(timer.checkNum(tmp)) {
+            this.break = Number(tmp);
+        } else {
+            timer.createToast('输入的应为1-60的整数数字');
+        }
+        timer.halt();
+        this.render(this.session, this.break);
+    },
+    startSession: function () {
+        let min, sec, z;
+        let leftSec = this.session * 60; // 剩余秒数
+        let fullSec = leftSec; // 总秒数
+        this.state = 'session';
+        $('#state').text('session');
+        $('#left-time').text(timer.createTimeText(leftSec));
+        $('#time-counting').css('height', 0).toggleClass('session').toggleClass('break');
+        this.sessionId = setInterval(function () { // 1s 执行一次
+            if(leftSec == 0) {
+                $('#left-time').text('');
+                clearInterval(timer.sessionId);
+                timer.startBreak();
+                return ;
+            }
+            if(timer.isPaused) {
+                return ;
+            } else {
+                leftSec--;
+                $('#left-time').text(timer.createTimeText(leftSec));
+                $('#time-counting').css('height', (fullSec - leftSec) / fullSec * 100 + '%');
+            }
+
+        }, 1000)
+    },
+    startBreak: function () {
+        let min, sec, z;
+        let leftSec = this.break * 60; // 剩余秒数
+        let fullSec = leftSec;
+        this.state = 'break';
+        $('#state').text('break');
+        $('#left-time').text(timer.createTimeText(leftSec));
+        $('#time-counting').css('height', 0).toggleClass('session').toggleClass('break');
+        this.breakId = setInterval(function () { // 1s 执行一次
+            if(leftSec == 0) {
+                $('#left-tim').text('');
+                clearInterval(timer.breakId);
+                timer.startSession();
+                return ;
+            }
+            if(timer.isPaused) {
+                return ;
+            } else {
+                leftSec--;
+                $('#left-time').text(timer.createTimeText(leftSec));
+                $('#time-counting').css('height', (fullSec - leftSec) / fullSec * 100 + '%');
+            }
+
+        }, 1000)
+    },
+    pause: function () {
+        $('#btn-pause').text(timer.isPaused ? '暂停' : '恢复');
+        timer.isPaused = !timer.isPaused;
+    },
+    toggleState: function () { // 改变状态
+        this.state = this.state == 'session' ? 'break' : 'session';
+    },
+    checkNum: function (text) { // 校验输入是否合法
+        let re = /^[0-9]+$/;
+        return $.isNumeric(text) && text < 60 && text > 1;
+    },
+    render: function (s, b) {
+        $('#session').text(s);
+        $('#break').text(b);
+    },
+    halt: function () {
+        switch (timer.state) {
+            case 'session':
+                clearInterval(timer.sessionId);
+                break;
+            case 'break':
+                clearInterval(timer.breakId);
+        }
+    },
+    createTimeText: function (sec) {
+        let m = parseInt(sec / 60),
+            s = parseInt(sec % 60);
+        return timer.paddingZero(m) + ':' + timer.paddingZero(s);
+    },
+    paddingZero: function (x) {
+        return x < 10 ? '0' + x : x;
+    },
+    createToast: function (text) {
+        let data = {message: text};
+        let snackbarContainer = document.querySelector('#mdl-toast');
+        snackbarContainer.MaterialSnackbar.showSnackbar(data);
+    }
+};
+
+timer.render(timer.session, timer.break);
+
+btnSession.click(function () {
+    timer.setSession();
+});
+
+btnBreak.click(function () {
+    timer.setBreak();
+});
+
+$('#btn-start').click(function () {
+   timer.startSession();
+});
+
+$('#btn-pause').click(function () {
+    timer.pause();
+});
+
+$('#btn-halt').click(function () {
+    timer.halt();
+});
+
+$('.clock-display').click(function () {
+    timer.pause();
+});
+
