181e0b64ac0a615128151eac77fff78e2ed99f52
kiling <zqling43@gmail.com>
2018-07-04 16:13:39 +0800

    audio preload fix
    预加载音频, 避免出现播放卡顿或者不播放的情况

--- FCC_Simon_Game/scripts/simon.js
+++ FCC_Simon_Game/scripts/simon.js
@@ -11,10 +11,10 @@ let game = {
     isWin: false, // 给 strict mode 用, 当为 strict mode 且通关时, 置该值为 true, 给 end() 判断
     currentMoves: [], // 玩家在当前的 count 下点击色块的顺序
     objSound: { // data-colorid 对应声音
-        0: 'audio/1do.mp3',
-        1: 'audio/2re.mp3',
-        2: 'audio/3mi.mp3',
-        3: 'audio/4fa.mp3'
+        0: new Audio('audio/1do.mp3'),
+        1: new Audio('audio/2re.mp3'),
+        2: new Audio('audio/3mi.mp3'),
+        3: new Audio('audio/4fa.mp3')
     },
     objColorId: { // data-colorid 对应id
         0: '#blue',
@@ -57,13 +57,18 @@ let game = {
                 $("#strict-status").toggleClass("strict-on");
                 game.strictMode = !game.strictMode;
             }
-        })
+        });
+        audioPreload(game.objSound);
     },
     check: function (colorid) { // 点击了色块以后, 判断当前点击色块是否与保存的 moves 的对应 value 符合
-        let audio = new Audio();
-        audio.src = game.objSound[colorid];
-        audio.load();
-        audio.play();
+        // let audio = new Audio();
+        // audio.src = game.objSound[colorid];
+        // audio.load();
+        // audio.play();
+        // console.log(colorid);
+        // game.objSound[colorid].load();
+        game.objSound[colorid].play();
+        game.objSound[colorid].currentTime = 0; // 把当前播放时间归零, 如果下次还是一样的音频时才会播
         // 判断当前点击色块结果与 moves 对比
         if(this.currentMoves[this.currentMoves.length - 1] == this.moves[this.currentMoves.length - 1]) { // 当前色块与 moves 对应
             if(this.currentMoves.length == this.moves.length) { // 当前 count 结束，进入下一个 count
@@ -129,9 +134,12 @@ let game = {
             let i = 0, audio = new Audio();
             game.clr  = setInterval(function () {
                 $(game.objColorId[moves[i]]).toggleClass('bright');
-                audio.src = game.objSound[moves[i]];
-                audio.load();
-                audio.play();
+                // audio.src = game.objSound[moves[i]];
+                // audio.load();
+                // audio.play();
+                // game.objSound[Number(moves[i])].load();
+                game.objSound[Number(moves[i])].play();
+                game.objSound[Number(moves[i])].currentTime = 0; // 把当前播放时间归零, 如果下次还是一样的音频时才会播
                 (function(i){
                     setTimeout(function () {
                         $(game.objColorId[moves[i]]).toggleClass('bright');
@@ -221,5 +229,11 @@ function mouseupEvent(evt) {
     if(game.isMouseDown == true) {
         $(game.tmp).removeClass("bright");
         game.isMouseDown = false;
+    }
+}
+
+function audioPreload(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        arr[i].load();
     }
 }
