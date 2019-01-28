40e84410622606bec3a3f545e804a1117df2555b
kiling <zqling43@gmail.com>
2019-01-28 20:50:43 +0800

    add weibo share
    增加分享到新浪微博功能

--- FCC_saying/script.js
+++ FCC_saying/script.js
@@ -3,28 +3,25 @@
  */
 
 window.onload = function () {
-    console.log('test');
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://v1.hitokoto.cn?c=a');
-  xhr.onreadystatechange = function () {
-      if(xhr.readyState == 4) {
-          console.log(xhr.responseText);
-          document.querySelector('#saying').innerText = JSON.parse(xhr.responseText)['hitokoto'];
-          document.querySelector('#author').innerText = '-- ' + JSON.parse(xhr.responseText)['from'];
-      }
-  };
-  xhr.send();
+    getSaying();
 };
 
 let btnRefresh = document.getElementById('refresh');
 btnRefresh.addEventListener('click', function () {
+    getSaying();
+});
+
+function getSaying() {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://v1.hitokoto.cn/?c=a');
+    xhr.open('GET', 'https://v1.hitokoto.cn');
     xhr.onreadystatechange = function () {
         if(xhr.readyState == 4) {
             document.querySelector('#saying').innerText = JSON.parse(xhr.responseText)['hitokoto'];
             document.querySelector('#author').innerText = '-- ' + JSON.parse(xhr.responseText)['from'];
+            document.title = JSON.parse(xhr.responseText)['hitokoto'] + '-- ' + JSON.parse(xhr.responseText)['from'];
+            let shareUrl = 'http://service.weibo.com/share/share.php?url=' + document.URL + '&type=button&language=zh_cn&appkey=3675162588&searchPic=false&style=simple&title=' + document.title;
+            document.getElementById('share').setAttribute('href', shareUrl);
         }
     };
     xhr.send();
-});
+}
